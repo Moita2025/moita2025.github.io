2faae88c552bbaf0759ab6a76678bccbcae3f21c
Moita2025 <2380758219@qq.com>
2025-11-22 22:07:59 +1300

    feat: Add PC `ESC` & Android `back` exit support

--- docs/assets/javascripts/dialog.js
+++ docs/assets/javascripts/dialog.js
@@ -5,10 +5,18 @@ function showDialog(htmlContent = "") {
     `<button onclick="hideDialog()" class="ui-btn">关闭</button>`;  // 设置动态内容
   }
   document.querySelector(".dialog-backdrop").style.display = "flex";
+
+  // 推入一个假历史记录，用于安卓返回键关闭对话框
+  history.pushState({ dialog: true }, "");
 }
 
 function hideDialog() {
   document.querySelector(".dialog-backdrop").style.display = "none";
+
+  // 关闭对话框时撤销虚假的 history
+  if (history.state && history.state.dialog) {
+    history.back();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,4 +25,18 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="dialog-box md-typeset" onclick="event.stopPropagation()"></div>
     </div>
   `);
+});
+
+// 电脑 ESC 键关闭
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    hideDialog();
+  }
+});
+
+window.addEventListener("popstate", (event) => {
+  // 若返回键触发，并且当前是 dialog 状态，则关闭对话框
+  if (event.state && event.state.dialog) {
+    hideDialog();
+  }
 });
